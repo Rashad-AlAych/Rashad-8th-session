bb558a6cdc88284f4337c95612cb5f33d27ef2ac
Rashad-AlAych <104849343+Rashad-AlAych@users.noreply.github.com>
2022-06-08 13:46:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-fetch('https://rashadapp0.herokuapp.com/data')
+fetch('https://rashadapp0.herokuapp.com/services')
   .then(response => response.json())
   .then(json => {
-          SelectStudent = document.getElementById('Students');
-      json.Sheet1.forEach(element => {
-          var option = document.createElement('option');
-          option.innerHTML = element.A;
-          SelectStudent.appendChild(option);
+          Selectdiv = document.getElementById('Maincontainer');
+          console.log(json.Services)
+          var originalcontent = `
+            <div class="icon"><i class=[ICON]></i></div>
+            <h4 class="title"><a href="">[Title]</a></h4>
+            <p class="description">[Description]</p>
+          </div>`
 
-      });
-  })
+     json.Services.forEach(element => {
+           var currentcontent = originalcontent;
+           currentcontent = currentcontent.replace('[Title]', element.A)
+           currentcontent = currentcontent.replace('[Description]', element.B)
+           currentcontent = currentcontent.replace('[ICON]', element.C)
+           var div = document.createElement('div');
+           div.className = "col-lg-4 col-md-6 icon-box";
+           div.innerHTML = currentcontent;
+           Selectdiv.appendChild(div);
+     })
+
+    });
